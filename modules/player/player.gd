ef33c8366e27a62b9c050767c8b3cc154d901e11
COOKIE-POLICE <109114410+COOKIE-POLICE@users.noreply.github.com>
2024-10-04 08:59:47 -0700

    Add weapon bob

--- modules/player/player.gd
+++ modules/player/player.gd
@@ -2,16 +2,31 @@ extends CharacterBody3D
 
 
 const SPEED: float = 5.0
-var sensitivity: float = 0.002
+var sensitivity: float = 0.004
+@onready var default_hand_position: Vector3 = %Hand.position
+var bob_amount_walking: Vector2 = Vector2(0.01, 0.01)
+var bob_freq_walking: Vector2 = Vector2(0.005, 0.01)
+var bob_amount_idle: Vector2 = Vector2(0.004, 0.004)
+var bob_freq_idle: Vector2 = Vector2(0.004, 0.004)
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation.y = rotation.y - event.relative.x * sensitivity
-		%Camera3D.rotation.x = %Camera3D.rotation.x - event.relative.y * sensitivity
-		%Camera3D.rotation.x = clamp(%Camera3D.rotation.x, deg_to_rad(-70), deg_to_rad(80))
+		%Head.rotation.x = %Head.rotation.x - event.relative.y * sensitivity
+		%Head.rotation.x = clamp(%Head.rotation.x, deg_to_rad(-70), deg_to_rad(80))
 
+func _weapon_bob(delta: float) -> void:
+	if velocity != Vector3.ZERO:
+		%Hand.position.y = lerp(%Hand.position.y, default_hand_position.y + sin(Time.get_ticks_msec() * bob_freq_walking.y) * bob_amount_walking.y, 10 * delta)
+		%Hand.position.x = lerp(%Hand.position.x, default_hand_position.x + sin(Time.get_ticks_msec() * bob_freq_walking.x) * bob_amount_walking.x, 10 * delta)
+	else:
+		%Hand.position.y = lerp(%Hand.position.y, default_hand_position.y + sin(Time.get_ticks_msec() * bob_freq_idle.y) * bob_amount_idle.y, 10 * delta)
+		%Hand.position.x = lerp(%Hand.position.x, default_hand_position.x + sin(Time.get_ticks_msec() * bob_freq_idle.x) * bob_amount_idle.x, 10 * delta)
+		
+		
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
@@ -33,4 +48,5 @@ func _physics_process(delta: float) -> void:
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	velocity.x = direction.x * SPEED
 	velocity.z = direction.z * SPEED
+	_weapon_bob(delta)
 	move_and_slide()
